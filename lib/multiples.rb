514c5b3e612f02a2d12064984d667d02ff45ad93
babyshoes <dianecai@gmail.com>
2015-10-12 10:35:17 -0400

    all green

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,24 @@
 # Enter your procedural solution here!
+# def sum_multiples_of_three_and_five(num)
+#   i = sum = 0
+#   while i < num
+#     sum += i if i%15 == 0 || i%5 == 0 || i%3 == 0
+#     i += 1
+#   end
+#   sum
+# end
+#
+# alternatively,
+# def sum_multiples_of_three_and_five(num)
+#   [*1...num].inject(0){ |sum, i| (i%15 == 0 || i%5 == 0 || i%3 == 0) ? sum + i : sum}
+# end
+#
+# sum_multiples_of_three_and_five(1000)
+
+def collect_multiples(limit)
+  [*1...limit].select{|i| i%15 == 0 || i%5 == 0 || i%3 == 0 }
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).inject(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,15 @@
 # Enter your object-oriented solution here!
+class Multiples
+  attr_accessor :limit
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    [*1...limit].select{|i| i%15 == 0 || i%5 == 0 || i%3 == 0 }
+  end
+
+  def sum_multiples
+    collect_multiples.inject(:+)
+  end
+end
